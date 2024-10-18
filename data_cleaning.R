3d39d2d407d989f1c8a459ca1da39dcebc5798f5
Courtney Cooper <courtneyfaycooper@gmail.com>
2024-10-18 14:29:33 -0500

    updates fri

--- data_cleaning.R
+++ data_cleaning.R
@@ -38,7 +38,24 @@ delta <- fulldat[!fulldat$region_Delta == 0, ]
 
 factors <- subset(fulldat, select = c(4,149:164,173))
 
-factors %<>% mutate(t2 = fv_top_three_factors) %>% separate_rows(fv_top_three_factors, sep = ",")
+factor_long <- factors %>% separate_rows(fv_top_three_factors, sep = ",")  %>%
+  mutate(fv_top_three_factors = as.numeric(fv_top_three_factors))
 
+df_wide <- factor_long %>%
+  mutate(value = 1) %>%  # Add a column for binary indicator (1 for presence)
+  pivot_wider(names_from = fv_top_three_factors, values_from = value, values_fill = 0, names_glue = "value_{fv_top_three_factors}")  # Fill missing values with 0
 
+delta_factors <- df_wide[!df_wide$region_Delta == 0, ]
+df_wide$region_Delta <- as.factor(df_wide$region_Delta)
 
+df_wide_sum <- df_wide %>% group_by(region_Delta) %>%
+  summarise(count_1 = sum(value_1),
+            count_2 = sum(value_2),
+            count_3 = sum(value_3),
+            count_4 = sum(value_4),
+            count_5 = sum(value_5),
+            count_6 = sum(value_6),
+            count_7 = sum(value_7))
+
+df_wide_sum[1,2:8] <- df_wide_sum[1, ] / 4709
+df_wide_sum[2,2:8] <- df_wide_sum[2, ] / 343
